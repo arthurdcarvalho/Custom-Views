c9f2e6d3e2b7659adbebf65b91efce7db92aab41
Arthur Carvalho <arthur.dcarvalho54@gmail.com>
2019-05-17 16:05:49 -0300

    Id para os Enums

--- app/src/main/java/br/com/arthur/customviews/MaskTypes.kt
+++ app/src/main/java/br/com/arthur/customviews/MaskTypes.kt
@@ -8,12 +8,18 @@ enum class MaskTypes
  * @param unmaskCount Text length with no masked
  * @param maskCount Text length with mask
  */
-constructor(private val maskName: String, val maskFormat: String, private val unmaskCount: Long, val maskCount: Long) {
-    CPF("CPF", "###.###.###-##", 11, 14),
-    CEP("CEP", "#####-###", 8, 9),
-    CELLPHONE_MASK("Cell Phone Mask", "(##) #####-####", 11, 12),
-    PHONE_MASK("Phone Mask", "### #### ####", 11, 13),
-    CREDIT_CARD_NUMBER_MASK("Credit Card Number Mask", "#### #### #### ####", 16, 19),
-    CREDIT_CARD_EXPIRATION_MASK("Credit Card Expiration Mask", "##/####", 6, 7),
-    DATE("Date", "##/##/####", 8, 10)
+constructor(
+    val id: Int,
+    val maskName: String,
+    val maskFormat: String,
+    val unmaskCount: Long,
+    val maskCount: Long
+) {
+    CPF(1, "CPF", "###.###.###-##", 11, 14),
+    CEP(2, "CEP", "#####-###", 8, 9),
+    CELLPHONE_MASK(3, "Cell Phone Mask", "(##) #####-####", 11, 12),
+    PHONE_MASK(4, "Phone Mask", "### #### ####", 11, 13),
+    CREDIT_CARD_NUMBER_MASK(5, "Credit Card Number Mask", "#### #### #### ####", 16, 19),
+    CREDIT_CARD_EXPIRATION_MASK(6, "Credit Card Expiration Mask", "##/####", 6, 7),
+    DATE(7, "Data", "##/##/####", 8, 10)
 }
