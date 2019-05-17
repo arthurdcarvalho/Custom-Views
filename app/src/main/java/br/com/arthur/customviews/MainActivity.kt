a067af8f9e4589280f5a7fea4d0c26279bccd915
Arthur Carvalho <arthur.dcarvalho54@gmail.com>
2019-05-17 15:41:17 -0300

    Configurando Listener para o botão do componente customizado e iniciando pelos ID

--- app/src/main/java/br/com/arthur/customviews/MainActivity.kt
+++ app/src/main/java/br/com/arthur/customviews/MainActivity.kt
@@ -2,11 +2,32 @@ package br.com.arthur.customviews
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import br.com.arthur.customviews.views.CompostCustomView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var compostCustomView: CompostCustomView
+    private lateinit var inputValue: TextView
+
+    private lateinit var value: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponents()
     }
+
+    private fun initComponents() {
+        compostCustomView = findViewById(R.id.compostCustomView)
+        inputValue = findViewById(R.id.inputValue)
+
+        compostCustomView.setButtonSendListener(View.OnClickListener {
+            value = compostCustomView.getInputWithNoMask()
+            inputValue.text = value
+        })
+    }
+
 }
